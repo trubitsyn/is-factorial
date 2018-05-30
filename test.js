e4f3d49a9cb42ee1b2eb48f35005f5e1825d09af
Nikola Trubitsyn <nikola.trubitsyn@gmail.com>
2018-05-31 00:02:04 +0300

    Add semicolons

--- test.js
+++ test.js
@@ -20,24 +20,24 @@ const assert = require('assert');
 const isFactorial = require('./');
 
 it('should match factorials', function() {
-    assert(isFactorial('1'))
-    assert(isFactorial('2'))
-    assert(isFactorial('120'))
+    assert(isFactorial('1'));
+    assert(isFactorial('2'));
+    assert(isFactorial('120'));
     assert(isFactorial('720'))
-})
+});
 
 it('should not match factorials', function() {
-    assert(!isFactorial(undefined))
-    assert(!isFactorial(null))
-    assert(!isFactorial(false))
-    assert(!isFactorial({}))
-    assert(!isFactorial(function() {}))
+    assert(!isFactorial(undefined));
+    assert(!isFactorial(null));
+    assert(!isFactorial(false));
+    assert(!isFactorial({}));
+    assert(!isFactorial(function() {}));
 
-    assert(!isFactorial('-1'))
-    assert(!isFactorial('-1000'))
-    assert(!isFactorial('256'))
-    assert(!isFactorial('2000'))
-    assert(!isFactorial('NaN'))
-    assert(!isFactorial('.'))
+    assert(!isFactorial('-1'));
+    assert(!isFactorial('-1000'));
+    assert(!isFactorial('256'));
+    assert(!isFactorial('2000'));
+    assert(!isFactorial('NaN'));
+    assert(!isFactorial('.'));
     assert(!isFactorial('?'))
-})
+});
